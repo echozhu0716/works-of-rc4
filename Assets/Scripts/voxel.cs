583ca737a9d46ee9b482e37facdc8d1b4c13a0b8
echoecho0716 <73584342+echoecho0716@users.noreply.github.com>
2020-10-31 02:03:27 +0800

    oct 29   voxelgrid exercise
    Raycast functionality and voxel status

---
+++ Assets/Scripts/voxel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//enem _u can use to add like diifferent status or different positions or whatever
+public enum VoxelState { Dead, Alive }
+
+
+
+
+public class Voxel 
+{
+    private Vector3Int _index;
+    private GameObject _goVoxel;
+    private VoxelState _status = VoxelState.Alive;
+    
+
+    //here use getters and setters   to  execute when you get variable    advantage of using get and set is that we can add some side effects.
+    public VoxelState Status     //try to access the information that is inside status, it will run the code below 
+    {
+        get
+        {
+            return _status;                    //use my private fields
+        }
+        set
+        {
+            _goVoxel.SetActive(value == VoxelState.Alive);
+            _status = value; 
+        }
+    }
+
+
+
+    public Voxel (Vector3Int index, GameObject goVoxelPrefab)
+    {
+        _index = index;
+        _goVoxel = GameObject.Instantiate(goVoxelPrefab, _index, Quaternion.identity);  //Quaternion add rotation
+        _goVoxel.GetComponent<VoxelTrigger>().TriggerVoxel = this;
+    }
+        
+}
+
---
+++ Assets/Scripts/voxelgrids.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class voxelgrids : MonoBehaviour
+{
+    [SerializeField]    // means the fied is accessible with unity
+    private Vector3Int GridDimension = new Vector3Int(16, 26, 6);
+
+    private Voxel[,,] _voxels;
+    private GameObject _goVoxelPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _goVoxelPrefab = Resources.Load("prefabs/VoxelCube") as GameObject;
+        CreatevoxelGrids();
+
+        /*dog barry = new dog("Barry", "My dog");
+        dog eddie = new dog("Eddie", "His dog");
+
+
+        barry.Bark(6);
+        eddie.Bark(2);*/
+
+
+
+
+    }
+
+    private void CreatevoxelGrids()
+
+    {
+        _voxels = new Voxel[GridDimension.x, GridDimension.y, GridDimension.z];  // how big is your area  how many elements are there
+
+        for (int x = 0; x < GridDimension.x; x++)
+        {
+            for (int y = 0; y < GridDimension.y; y++)
+            {
+                for (int z = 0; z < GridDimension.z; z++)
+                {
+                    _voxels[x, y, z] = new Voxel(new Vector3Int(x, y, z), _goVoxelPrefab);  // literal index, loop over all the analysis -element in loop
+                }
+            }
+        }
+
+    }
+
+
+        // Update is called once per frame
+
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                HandleRaycast();
+            }
+
+        }
+
+
+        private void HandleRaycast()
+        {
+            Debug.Log("Clicked");
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //then let the ray hit sth
+        if(Physics.Raycast(ray,out RaycastHit hit))
+        {
+
+            // transform-everyobject has a transform to get them in space
+
+           Transform objectHit = hit.transform;
+
+            if (objectHit.CompareTag("Voxel"))
+            {
+                // Destroy(objectHit.gameObject);
+                objectHit.gameObject.GetComponent<VoxelTrigger>().TriggerVoxel.Status = VoxelState.Dead;
+
+            }
+        }
+
+        }
+
+
+    
+
+}
+
+
